29e0b1ccf9aaf02b902515a590102b886bb7641d
FonjiDaniel <fonjidaniel28@gmail.com>
2023-06-17 21:55:11 +0100

    new backend
    new backend

---
+++ react-portfoliobackend/config/app.config.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const MONGO_DB_CONFIG = {
+    DB: 'mongodb+srv://fonjidaniel82:Sn0Zac4HGNnEAkWk@reactbackendportfolio.d8qsjf2.mongodb.net/ReactbackendProject?retryWrites=true&w=majority'
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
