540544a23185c714680e8eb8d6ea964064a97630
FonjiDaniel <fonjidaniel28@gmail.com>
2023-06-18 06:39:39 +0100

    deleted

--- router/app.router.js
+++
@@ -1,9 +0,0 @@
-const controller = require ("../controller/appcontroller");
-const router= require("express").Router();
-
-router.get("/portfolio",controller.portfolioAppRoute);
-router.get("/testimonial", controller.testimonialAppRoute);
-
-module.exports = router
-
-
